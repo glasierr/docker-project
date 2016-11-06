becb244622edfc4c8b147623d93164a83dd8b4ed
Vladyslav Vlasenko <glasierrvlas@gmail.com>
2016-11-06 13:53:25 +0200

    Start using in memory H2 database instead of MongoDB

--- drivers-storage/src/main/java/vlasenko/violations/drivers/repository/DriversRepository.java
+++ drivers-storage/src/main/java/vlasenko/violations/drivers/repository/DriversRepository.java
@@ -1,12 +1,12 @@
 package vlasenko.violations.drivers.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vlasenko.violations.drivers.domain.Driver;
 
 import java.util.Optional;
 
 @Repository
-public interface DriversRepository extends MongoRepository<Driver, String> {
+public interface DriversRepository extends JpaRepository<Driver, String> {
     Optional<Driver> findByLicenceId(String id);
 }
